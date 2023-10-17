e8acda0d9f6203772b5b1c5d6dc71d69c8a8acfe
MatiasBarreto1595 <matias.barreto98@hotmail.com>
2023-10-17 12:28:57 -0300

    crearNuevoArticulo

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,24 +1,47 @@
 const { Author, Article, Comment } = require("../models");
+const formidable = require("formidable");
 
 // Display a listing of the resource.
 
-async function index(req, res) {
-
-}
+async function index(req, res) {}
 
 // Display the specified resource.
 async function show(req, res) {
-  const article = await Article.findByPk(req.params.id, { include: [Author,{model: Comment, include: Author}] });
+  const article = await Article.findByPk(req.params.id, {
+    include: [Author, { model: Comment, include: Author }],
+  });
   const title = article.title;
   const text = `Creado por ${article.author.firstname} ${article.author.lastname} el ${article.createdAt}`;
   res.render("articles", { title: title, text: text, article });
 }
 
 // Show the form for creating a new resource
-async function create(req, res) {}
+async function create(req, res) {
+  const authors = await Author.findAll();
+  console.log(authors);
+  res.render("new-article", { authors });
+}
 
 // Store a newly created resource in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  console.log("recibí peticion en articulos POST");
+  const form = formidable({
+    multiples: true,
+    uploadDir: __dirname + "/../public/img",
+    keepExtensions: true,
+  });
+  form.parse(req, async (err, fields, files) => {
+    if (err) return res.send("Algo falló intentá más tarde..");
+    await Article.create({
+      title: fields.title,
+      content: fields.content,
+      image: files.image.newFilename,
+      authorId: fields.authorId,
+    });
+    // Hacer algo con fields y files...
+    res.redirect("admin");
+  });
+}
 
 // Show the form for editing the specified resource.
 async function edit(req, res) {
