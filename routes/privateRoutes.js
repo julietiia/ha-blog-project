e7daedfaf73c8c0c6f53961d8bc1eab899903652
gonzaxav <68866316+gonzaxav@users.noreply.github.com>
2023-10-18 17:02:50 -0300

    auth added middleware for private routes

--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const pagesController = require("../controllers/pagesController");
+const ensureIsAuthenticated = require("../middlewares/ensureIsAuthenticated");
 
 // Rutas relacionadas al panel de control (Admin):
 // ...
+
+router.get("/", ensureIsAuthenticated, pagesController.showAdmin);
 
 module.exports = router;
